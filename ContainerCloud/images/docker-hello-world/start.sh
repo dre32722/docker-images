89f27cb4afa0bf9e1d0f764e7fbb09f1e1110124
dre32722 <dre32722@users.noreply.github.com>
2017-03-09 22:24:51 +1030

    Update start.sh
    Added echo for date and uname

--- ContainerCloud/images/docker-hello-world/start.sh
+++ ContainerCloud/images/docker-hello-world/start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-node -v
+
 sed -i -e s/HOSTNAME/${HOSTNAME}/g /usr/share/nginx/html/index.html
 
 # Use `exec` so that PID 1 is the nginx process and not this script
+exec echo `uname -a`
+exec echo `date`
 exec /usr/sbin/nginx -g "daemon off;"
