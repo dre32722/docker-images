2919ec87b58bd859822c28d60ef394a32de2ca9d
dre32722 <dre32722@users.noreply.github.com>
2017-03-09 22:04:10 +1030

    Update start.sh
    added node -v , removed exec bash

--- ContainerCloud/images/docker-hello-world/start.sh
+++ ContainerCloud/images/docker-hello-world/start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-
+node -v
 sed -i -e s/HOSTNAME/${HOSTNAME}/g /usr/share/nginx/html/index.html
 
 # Use `exec` so that PID 1 is the nginx process and not this script
 exec /usr/sbin/nginx -g "daemon off;"
-exec bash
