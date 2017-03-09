31865741b26886f012c8f241a762668bded599ab
dre32722 <dre32722@users.noreply.github.com>
2017-03-09 22:40:30 +1030

    Update start.sh
    commented out exec statements that didnt work

--- ContainerCloud/images/docker-hello-world/start.sh
+++ ContainerCloud/images/docker-hello-world/start.sh
@@ -3,6 +3,6 @@
 sed -i -e s/HOSTNAME/${HOSTNAME}/g /usr/share/nginx/html/index.html
 
 # Use `exec` so that PID 1 is the nginx process and not this script
-exec echo `uname -a`
-exec echo `date`
+# exec echo `uname -a`
+# exec echo `date`
 exec /usr/sbin/nginx -g "daemon off;"
